b42b614c0a549ad1b4ca01a35308ebeda4f4aaa3
ferhatwi <87044532+ferhatwi@users.noreply.github.com>
2022-03-22 01:54:38 +0300

    Hello World

---
+++ README.md
@@ -0,0 +1,7 @@
+# Supabase BOM
+Platform dependency for Supabase libraries. Declaring this is not required, but strongly recommended.
+## Download
+```
+dependencies {
+ implementation "io.github.ferhatwi:supabase-kt-bom:0.1.0"
+} 
---
+++ lib/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    `java-platform`
+    `maven-publish`
+    signing
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+            credentials {
+                username = System.getenv("OSSRH_USERNAME")
+                password = System.getenv("OSSRH_TOKEN")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("mavenJava") {
+            pom {
+                from(components["javaPlatform"])
+                groupId = "io.github.ferhatwi"
+                artifactId = "supabase-kt-bom"
+                version = "0.1.0"
+                name.set("Supabase BOM")
+                description.set("Supabase BOM")
+                url.set("http://www.github.com/ferhatwi/supabase-kt-bom")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("ferhatwi")
+                        name.set("Ferhat")
+                        email.set("ferhatyigit7@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/ferhatwi/supabase-kt-bom.git")
+                    developerConnection.set("scm:git:ssh://github.com/ferhatwi/ferhatwi.git")
+                    url.set("http://github.com/ferhatwi/supabase-kt-bom/")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+repositories {
+    mavenCentral()
+}
+
+javaPlatform {
+    allowDependencies()
+}
+
+dependencies {
+    constraints {
+        api("io.github.ferhatwi:supabase-auth-kt:0.1.0")
+        api("io.github.ferhatwi:supabase-storage-kt:0.1.0")
+        api("io.github.ferhatwi:supabase-database-kt:0.3.0")
+    }
+
+    api("io.ktor:ktor-client-core:1.6.7")
+    api("io.ktor:ktor-client-cio:1.6.7")
+    api("io.ktor:ktor-client-serialization:1.6.7")
+    api("io.ktor:ktor-client-gson:1.6.7")
+}
